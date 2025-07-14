ff1e408b88f0cc520835c5e58cd53fde8e10afc0
fiddleswe <smartsoodprasert69@gmail.com>
2025-07-14 23:18:12 +0700

    Minor update for UI design and frontpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./index.css";
-import Nav from "./components/Header";
+import Nav from "./components/Nav";
 import Main from "./components/Main";
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col gap-4 pt-[170px]">
       <Nav />
+      <Header />
       <Main />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,16 @@
 import React from "react";
-import { IoMdSearch } from "react-icons/io";
-import { IoIosArrowDown } from "react-icons/io";
 
-// Tailwind Classes Group
-const iconClass =
-  "absolute left-3 top-1/2 -translate-y-1/2 text-[22px] text-white text-opacity-60";
+const profileUser = "h-[17vh] w-[17vh] rounded-full bg-white/60 text-white"
+const usernameHolder = "text-[30px] font-medium text-white text-opacity-60"
 
-const navClass =
-  "left-0 top-0 z-50 w-full border-b-[1.5px] border-white/15 bg-black p-4 shadow-md";
-
-const ulClass =
-  "flex items-center justify-start gap-5 text-[14px] font-medium text-white text-opacity-60 ml-5";
-
-const inputClass =
-  "placeholder-white/60 text-white bg-[#2C2C2C] w-96 h-12 rounded-xl pl-11";
-
-const searchClass = "text-sm font-medium text-white text-opacity-60";
-
-//  Default Main Function
-
-export default function Nav() {
+export default function Header() {
   return (
-    <nav
-      className={`${navClass} fixed flex items-center justify-between`}
-    >
-      <ul className={ulClass}>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Dashboard</a>
-        </li>
-        <li>
-          <a href="#">Learning</a>
-        </li>
-        <li>
-          <a href="#">Documentation</a>
-        </li>
-      </ul>
-
-      <div
-        className={`${searchClass} absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2`}
-      >
-        <IoMdSearch className={iconClass} />
-        <input
-          type="text"
-          aria-label="Search course..."
-          placeholder="Find your course..."
-          className={inputClass}
-        />
-      </div>
-
-      <div className="mr-5 flex items-center gap-5">
-        <div className="h-[40px] w-[40px] rounded-full bg-white/60"></div>
-        <IoIosArrowDown className="text-[25px] text-white text-opacity-60" />
-      </div>
-    </nav>
+    <header className="flex items-center justify-center relative gap-10">
+        <div className={`${profileUser}`}>
+        </div>
+        <div className={`${usernameHolder}`}>
+            Username
+        </div>
+    </header>   
   );
 }
