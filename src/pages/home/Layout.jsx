fc99cdc1031398c7f1f2d8cd12373fcbb6a0a880
fiddleswe <smartsoodprasert69@gmail.com>
2025-07-22 23:01:16 +0700

    ux/ui update and additional features

--- src/pages/home/Layout.jsx
+++ src/pages/home/Layout.jsx
@@ -2,14 +2,12 @@ import Nav from "./Nav";
 import Header from "./Header";
 import Main from "./Main";
 
-export default function Home() {
+export default function HomeLayout() {
   return (
-    <div className="flex flex-col gap-4 pt-[170px]">
+    <div className="flex flex-col gap-4 pt-[300px]">
       <Nav />
       <Header />
       <Main />
     </div>
   );
 }
-
- ".pages/home/Layout"
--- src/pages/home/Main.jsx
+++ src/pages/home/Main.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
+const mainClass =
+  "flex flex-col gap-4 pt-[150px] font-normal text-white";
+
 export default function main() {
-    return (
-        <main>
-        </main>
-    );
+  return (
+    <main className={`${mainClass}`}>
+      <div>
+        <h1 className="ml-32 text-3xl font-bold">Registered course</h1>
+      </div>
+      <div className="mb-4 ml-32 mt-6 h-[30vh] w-[85%] rounded-xl bg-[#141414]"></div>
+    </main>
+  );
 }
